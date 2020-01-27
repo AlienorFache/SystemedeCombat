f6167fdafc95a20c46176b20e414018ec962684f
AlienorFache <59962539+AlienorFache@users.noreply.github.com>
2020-01-27 14:55:50 +0100

    Update perso.c
    correction

--- perso.c
+++ perso.c
@@ -40,16 +40,10 @@ void creationMonstre(struct Monstre tMonstre[], int nbMonstre)
 
         tMonstre[i].life = 1;
 
-        tMonstre[i].pm = (rand() % 20) + 1;
+        tMonstre[i].pm = 5;
 
-        if (tMonstre[i].pm > 10)
-        {
-            tMonstre[i].degats = (rand() % 5) + 1;
-        }
-        else
-        {
+        
             tMonstre[i].degats = (rand() % 11) + 10;
-        }
 
         if (tMonstre[i].degats > 15)
         {
@@ -169,38 +163,45 @@ void actionPerso(perso *perso, struct Monstre tMonstre[], struct Perso tPerso[])
     }
 }
 ////////////////////////////////////////////////////////////////////////////////
-void actionMonstre(struct Monstre tMonstre[], int nbMonstre){
+void actionMonstre(struct Monstre tMonstre[], struct Perso tPerso[], int nbMonstre){
 	int persoChoisi=(rand()%3)+1;
 	
 	for (int i = 0; i < nbMonstre; i++){
 		
 		if (tMonstre[monstreChoisi].agro = 1){
-			tPerso[i].life -= tMonstre[i].degats;
+			tPerso[2]->life -= tMonstre[i].degats;
 		}
 		else{
-			tMonstre[i].choix=(rand ()%3)+1;
+			if (tMonstre[i]->pm >=5) {
+				
+				//1 pour le soin, 2 pour l'attaque et 3 pour se protéger
+				tMonstre[i]->choix=(rand ()%3)+1; //entre 1 et 3
+			}
+			if (tMonstre[i]->pm < 5) {
+				
+				//2 pour l'attaque et 3 pour se protéger
+				tMonstre[i]->choix=(rand ()%2)+2; // entre 2 et 3
+			}
 			
-			if{ monstre->choix == 1){
-				tMonstre[i].life += 20;
-				if (tPerso[i].life > 100){
-                tPerso[i].life = 100;
+			if{ tMonstre[i]->choix == 1){
+				tMonstre[i]->life += 20;
+			if (tMonstre[i]->life > 100){
+                	tMonstre[i]->life = 100;
             }
-			monstre->pm -= 5;
-			printf("Le monstre se soigne, il a maintenant %d de points de vie", tMonstre[i].life);
+			tMonstre[i]->pm -= 5;
+			printf("Le monstre se soigne, il a maintenant %d de points de vie\n", tMonstre[i]->life);
 				
 			}
 			
-			if{ monstre->choix == 2){
+			if{ tMonstre[i]->choix == 2){
 				
-				tperso[persoChoisi].life -= 10;
-				monstre->pm -= 5;
-				printf("Le monstre attaque, vous avez maintenant %d de points de vie",tPerso[persoChoisi].life);
+				tperso[persoChoisi]->life -= 10 /tperso[persoChoisi]->resistance;
+				printf("Le monstre attaque, vous avez maintenant %d de points de vie\n",tPerso[persoChoisi].life);
 			}
 			
-			if(monstre->choix==3){
-				monstre->resistance *= 2;
-			printf("Le monstre resiste ! Il gagne %d de reistance", monstre->resistance);
-			printf("Il subira 2x moins de degats pendant se tour.\n");
+			if(tMonstre[i]->choix==3){
+				tMonstre[i]->resistance *= 2;
+			printf("Le monstre resiste ! Les degats qu'il recoit sont divises par %d. \n", tMonstre[i]->resistance);
 			}
 		}
 		
@@ -262,6 +263,8 @@ int main()
         printf("\nVous jouez le tank :\nPV : %d\nPM : %d\nDegats :%d\nResistance :%d\nAction speciale : agro\n", tank.life, tank.pm, tank.degat, tank.resistance);
         actionPerso(&tPerso[2], tMonstre, tPerso);
 
+	    
+	    actionMonstre(tMonstre, tPerso, nbMonstre);
         //apres le tour des monstres
         for (int i = 0; i < nbMonstre; i++)
         {
@@ -272,7 +275,6 @@ int main()
         mage.resistance = 1;
         archer.resistance = 1;
         tank.resistance = 2;
-        tMonstre[i].resistance=0;
     }
 
     return 0;
